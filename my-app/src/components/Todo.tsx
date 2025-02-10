4c301b161de3576b181659998dbe18aaca84a4c8
Pritam Kale <pritamtk236@gmail.com>
2025-02-10 16:01:32 +0530

    Add Edit Task Option

--- my-app/src/components/Todo.tsx
+++ my-app/src/components/Todo.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 const Todo = () => {
-  const [todoList, setTodoList] = useState<{ id: number; text?: string; isComplete: boolean; }[]>([]);
+const [todoList, setTodoList] = useState<{ id: number; text?: string; isComplete: boolean;}[]>([]);
 const inputRef=useRef<HTMLInputElement>(null);
 const[dateTime, setDateTime] = useState("");
 
+const completedCount = todoList.filter((todo) => todo.isComplete).length;
+const totalTodos = todoList.length;
+const progress = totalTodos > 0 ? (completedCount / totalTodos) * 100 : 0;
 
 const add=()=>{
 const InputText=inputRef.current?.value.trim();
@@ -34,8 +38,14 @@ const InputText=inputRef.current?.value.trim();
     const toggle = (id: number) => {
       setTodoList((prevTodos) =>
         prevTodos.map((todo) =>
-          todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
-        )
+          todo.id === id ? { ...todo, isComplete: !todo.isComplete} : todo
+        ) 
+      );
+    };
+
+    const editTodo = (id: number, newText: string) => {
+      setTodoList((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
       );
     };
 //Todo Date and Time
@@ -50,10 +60,10 @@ const InputText=inputRef.current?.value.trim();
     },[todoList])
     
   return (
-    <div className="w-full h-screen flex justify-center items-center mt-7 mb-10 bg-white">
-<div className='bg-white w-11/12 h-screen place-self-center max-w-4xl flex flex-col p-7 min-h-[450px] rounded-xl '>
+<div className="w-full h-screen flex justify-center items-center overflow-hidden mt-0 bg-slate-400">
+<div className='bg-slate-400 w-11/12 h-full place-self-center max-w-4xl flex flex-col p-2 min-h-[450px] rounded-xl'>
  {/* --------title------------ */}
- <div className="flex flex-col items-center mt-7">
+<div className="flex flex-col items-center mt-7">
   {/* To-Do List Heading with Icon */}
   <div className="flex items-center space-x-2">
     <img className="w-8" src={todo_icon} alt="To-Do Icon" />
@@ -63,18 +73,28 @@ const InputText=inputRef.current?.value.trim();
   {/* Date and Time */}
   <h2 className="font-semibold text-2xl mt-3 text-center">{dateTime}</h2>
 </div>
+{/* ---------Progress Bar--------- */}
+<div className="my-6">
+          <p className="text-lg font-semibold text-center">Progress: {Math.round(progress)}%</p>
+          <div className=" ml-36 mt-4 w-8/12 bg-gray-300 h-5 rounded-full">
+            <div
+              style={{ width: `${progress}%` }}
+              className="bg-green-500 h-full rounded-full"
+            ></div>
+          </div>
+        </div>
 
 {/* ---------input box--------- */}
-<div className="flex justify-center my-7">
-<div className='flex items-center my-7 bg-gray-200 rounded-full max-w-md '>
-    <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-12 w-full pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-    <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
+<div className="flex justify-center my-7 mt-3">
+<div className='flex items-center my-6 bg-gray-200 rounded-full max-w-lg w-full h-11'>
+    <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-11 w-11 pl-4 pr-4 placeholder:text-slate-600 text-xl' type="text" placeholder='Add your task' />
+    <button onClick={add} className='border-none rounded-full bg-orange-600 w-44 h-11 text-white text-2xl font-medium cursor-pointer pl-6'>Add +</button>
 </div>
 </div>
 {/* ---------To do list--------- */}
 <div>
   {todoList.map((item,index)=>{
-return <TodoItems key={index} text={item.text || "Default Todo"} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}  toggle={toggle} />
+return <TodoItems key={index} text={item.text || "Default Todo"} id={item.id}  isComplete={item.isComplete} editTodo={editTodo} deleteTodo={deleteTodo}  toggle={toggle} />
 
 })}
 </div>
