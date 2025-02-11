342df40c6ac3aced3655de842f2fd88fa9f6e1c8
Pritam Kale <pritamtk236@gmail.com>
2025-02-11 17:55:45 +0530

    Route Path Added TO Login Page

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import './index.css';
 import Middle from "./components/Middle";
+import Login from "./Login";
 
 
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Middle" element={<Middle />} />
+        <Route path="/Login" element={<Login />} />
         
       </Routes>
     </Router>
