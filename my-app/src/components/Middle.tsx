fb32265cc682bfce88b847b83a7eb523b0acc354
Pritam Kale <pritamtk236@gmail.com>
2025-02-19 14:34:08 +0530

    Middle Page Animation

--- my-app/src/components/Middle.tsx
+++ my-app/src/components/Middle.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Header from './Header'
-import Footer from '../Footer'
+import Footer from './Footer'
 import Todo from './Todo'
+import { motion } from "framer-motion";
 
 
 const Middle = () => {
   return (
+    <motion.div
+    initial={{ y: "-100vw", opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    exit={{ y: "100vw", opacity: 0 }}
+    transition={{ type: "spring", stiffness: 50, damping: 13 }}
+    
+  >
     <div>
       <Header/>
       <Todo/>
       <Footer/>
     </div>
-  )
+    </motion.div>
+  );
 }
 
 export default Middle
